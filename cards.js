965c7c6299225e4db5a4bd8fd4c73c28b08dbc48
Serhii Vernii <130190179+VernyStar@users.noreply.github.com>
2024-01-30 19:58:16 +0200

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,146 @@
+let AllCards = [
+  {
+    "suit": "hearts",
+    "name": "six",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "seven",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "eight",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "nine",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "ten",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "jack",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "queen",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "king",
+  }, 
+  {
+    "suit": "hearts",
+    "name": "ace",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "six",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "seven",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "eight",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "nine",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "ten",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "jack",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "queen",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "king",
+  }, 
+  {
+    "suit": "diamonds",
+    "name": "ace",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "six",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "seven",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "eight",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "nine",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "ten",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "jack",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "queen",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "king",
+  }, 
+  {
+    "suit": "clubs",
+    "name": "ace",
+  }, 
+  {
+    "suit": "spades",
+    "name": "six",
+  }, 
+  {
+    "suit": "spades",
+    "name": "seven",
+  }, 
+  {
+    "suit": "spades",
+    "name": "eight",
+  }, 
+  {
+    "suit": "spades",
+    "name": "nine",
+  }, 
+  {
+    "suit": "spades",
+    "name": "ten",
+  }, 
+  {
+    "suit": "spades",
+    "name": "jack",
+  }, 
+  {
+    "suit": "spades",
+    "name": "queen",
+  }, 
+  {
+    "suit": "spades",
+    "name": "king",
+  }, 
+  {
+    "suit": "spades",
+    "name": "ace",
+  }
+];
